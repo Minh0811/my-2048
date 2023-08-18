94e7c42e37f622f6ac35e87dfc677c3f54ae04bf
minh0811 <vokhaiminh0811@gmail.com>
2023-08-18 11:47:51 +0700

    Initial Commit

---
+++ my-2048/my_2048App.swift
@@ -0,0 +1,17 @@
+//
+//  my_2048App.swift
+//  my-2048
+//
+//  Created by Minh Vo Khai on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct my_2048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

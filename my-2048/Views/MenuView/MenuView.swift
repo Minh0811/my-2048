285b7e79c296a1598c26f510e1ab94999bc951ac
minh0811 <vokhaiminh0811@gmail.com>
2023-08-25 02:00:53 +0700

    add menu view

--- my-2048/Views/MenuView/MenuView.swift
+++ my-2048/Views/MenuView/MenuView.swift
@@ -9,7 +9,46 @@ import SwiftUI
 
 struct MenuView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("Select Difficulty")
+                .font(.largeTitle)
+                .padding()
+
+            Button(action: {
+          
+               // GameView()
+            }) {
+                Text("Game")
+                    .font(.title)
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+
+            Button(action: {
+             
+            }) {
+                Text("Choose dificult")
+                    .font(.title)
+                    .padding()
+                    .background(Color.orange)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+
+            Button(action: {
+               
+            }) {
+                Text("Setting")
+                    .font(.title)
+                    .padding()
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+        .padding()
     }
 }
 

40e8fdffd19de289574813dad8474eb5cb869176
minh0811 <vokhaiminh0811@gmail.com>
2023-08-27 20:14:04 +0700

    button in menu to game view

--- my-2048/Views/MenuView/MenuView.swift
+++ my-2048/Views/MenuView/MenuView.swift
@@ -9,46 +9,61 @@ import SwiftUI
 
 struct MenuView: View {
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Select Difficulty")
-                .font(.largeTitle)
-                .padding()
-
-            Button(action: {
-          
-               // GameView()
-            }) {
-                Text("Game")
-                    .font(.title)
+        NavigationView {
+            
+            VStack(spacing: 20) {
+                Text("Select Difficulty")
+                    .font(.largeTitle)
                     .padding()
-                    .background(Color.green)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+                
+//                Button(action: {
+//                    
+//                    GameView()
+//                    
+//                }) {
+//                    Text("Game")
+//                        .font(.title)
+//                        .padding()
+//                        .background(Color.green)
+//                        .foregroundColor(.white)
+//                        .cornerRadius(10)
+//                }
+                
+                NavigationLink(
+                    destination: GameView(),
+                    label: {
+                        Text("Game")
+                            .font(.title)
+                            .padding()
+                            .background(Color.green)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)                    }
+                )
+                Button(action: {
+                    
+                }) {
+                    Text("Choose dificult")
+                        .font(.title)
+                        .padding()
+                        .background(Color.orange)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                Button(action: {
+                    
+                }) {
+                    Text("Setting")
+                        .font(.title)
+                        .padding()
+                        .background(Color.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
             }
-
-            Button(action: {
-             
-            }) {
-                Text("Choose dificult")
-                    .font(.title)
-                    .padding()
-                    .background(Color.orange)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
-
-            Button(action: {
-               
-            }) {
-                Text("Setting")
-                    .font(.title)
-                    .padding()
-                    .background(Color.red)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }
+            .padding()
         }
-        .padding()
+        
     }
 }
 
